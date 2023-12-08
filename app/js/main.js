4ae1c9a0044196a4b38b29ac2b38d12483aade9d
kosm-dev <72253360+kosm-dev@users.noreply.github.com>
2023-12-08 04:10:02 +0200

    added header

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,25 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
+  if(document.querySelector('.burger')) {
+      const burgerButton = document.querySelector('.burger');
+
+      burgerButton.addEventListener('click', toggleBurger);
+
+      function toggleBurger() {
+        const headerMenu = document.querySelector('.header__menu'),
+          headerAuth = document.querySelector('.header__auth');
+
+        burgerButton.classList.toggle('active');
+        headerMenu.classList.toggle('active');
+        headerAuth.classList.toggle('active');
+        document.body.classList.toggle('lock');
+      }
+  }
+
+  
+
+
+
+
 });
